a2e22b4aeefb31eb7ae3db8c77d4449885463918
y-007 <nakaiy@hotmail.com>
2024-11-09 21:31:32 -0500

    Deploy to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,18 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/profile_sam/',  // Ensure this is correct
+  base: '/profile_sam/',
   build: {
     rollupOptions: {
-      input: 'index.html',  // Ensure this matches your entry file
+      input: 'index.html',
       output: {
         assetFileNames: 'assets/[name]-[hash][extname]',
       },
+      external: [
+        '/profile_sam/assets/index-DjStq1bM.css',
+        '/profile_sam/assets/index-wqGJMUur.js',
+      ],
     },
   },
 });
+ 
